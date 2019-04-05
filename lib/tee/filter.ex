aa4863983d01f95508c3a828d9a769a2c1361708
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2019-04-05 13:28:37 +0200

    delete logging from example element

--- lib/tee/filter.ex
+++ lib/tee/filter.ex
@@ -6,13 +6,6 @@ defmodule Membrane.Element.Tee.Filter do
     mode: :pull,
     demand_unit: :bytes,
     caps: :any,
-    options: [
-      divisor: [
-        type: :integer,
-        default: 1,
-        description: "Number by which the counter will be divided before sending notification"
-      ]
-    ]
 
   def_output_pad :output1,
     availability: :always,
@@ -25,50 +18,12 @@ defmodule Membrane.Element.Tee.Filter do
     caps: :any
 
   @impl true
-  def handle_init(%__MODULE{interval: interval}) do
-    state = %{
-      interval: interval,
-      counter: 0,
-      timer: nil
-    }
-
+  def handle_process(_pad, _payload, _ctx, state) do
     {:ok, state}
   end
 
   @impl true
-  def handle_prepared_to_stopped(_ctx, state) do
-    {:ok, :cancel} = :timer.cancel(state.timer)
-    {:ok, %{state | counter: 0, timer: nil}}
-  end
+  def handle_demand(_output_pad, _size, _unit, _ctx, state) do
+    {:ok, state}
 
-  @impl true
-  def handle_prepared_to_playing(_ctx, state) do
-    {:ok, timer} = :timer.send_interval(state.interval, :tick)
-    {:ok, %{state | timer: timer}}
-  end
-
-  @impl true
-  def handle_demand(:output, size, :bytes, _context, state) do
-    {{:ok, demand: {:input, size}}, state}
-  end
-
-  @impl true
-  def handle_process(:input, %Membrane.Buffer{} = buffer, _context, state) do
-    new_state = %{state | counter: state.counter + 1}
-    {{:ok, buffer: {:output, buffer}}, new_state}
-  end
-
-  @impl true
-  def handle_other(:tick, ctx, state) do
-    # create the term to send
-    notification = {
-      :counter,
-      div(state.counter, ctx.pads.input.options.divisor)
-    }
-
-    # reset the timer
-    new_state = %{state | counter: 0}
-
-    {{:ok, notify: notification}, new_state}
-  end
 end
