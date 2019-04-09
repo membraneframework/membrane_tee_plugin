3a897295267fb93dd3d1aa35ba5efce9ddd59ff0
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2019-04-09 15:40:02 +0200

    implement handle event and caps

--- lib/tee/filter.ex
+++ lib/tee/filter.ex
@@ -26,6 +26,22 @@ defmodule Membrane.Element.Tee.Filter do
   end
 
   @impl true
+  def handle_event(:input, event, ctx, state) do
+    {{:ok,
+      ctx.pads
+      |> Enum.filter(fn {_k, v} -> v.direction != :input end)
+      |> Enum.map(fn {k, _v} -> {:event, {k, event}} end)}, state}
+  end
+
+  @impl true
+  def handle_caps(:input, caps, ctx, state) do
+    {{:ok,
+    ctx.pads
+    |> Enum.filter(fn {_k, v} -> v.direction != :input end)
+    |> Enum.map(fn {k, _v} -> {:caps, {k, caps}} end)}, state}
+  end
+
+  @impl true
   def handle_demand(:output_static, size, _unit, _ctx, state) do
     {{:ok, demand: {:input, size}}, state}
   end
