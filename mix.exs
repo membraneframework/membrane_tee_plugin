562c45cde9d1dda9c5decb384841b0eaf8add537
AndrzejPodo <andrzejpodobinski2@gmail.com>
2021-11-18 10:56:44 +0100

    bump to 0.6.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Element.Tee.MixProject do
   use Mix.Project
 
-  @version "0.5.0"
+  @version "0.6.0"
   @github_url "https://github.com/membraneframework/membrane-element-tee"
 
   def project do
@@ -29,7 +29,7 @@ defmodule Membrane.Element.Tee.MixProject do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.7.0"},
+      {:membrane_core, "~> 0.8.0"},
       {:bunch, "~> 1.0"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: [:dev], runtime: false}
