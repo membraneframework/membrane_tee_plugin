b16b91afd390d11172e8b5fe9726e15ba8ad908e
Mateusz Front <mateusz.front@swmansion.com>
2022-02-16 12:01:58 +0100

    bump to v0.8.0

--- mix.exs
+++ mix.exs
@@ -1,14 +1,14 @@
 defmodule Membrane.Tee.MixProject do
   use Mix.Project
 
-  @version "0.7.0"
+  @version "0.8.0"
   @github_url "https://github.com/membraneframework/membrane_tee_plugin"
 
   def project do
     [
       app: :membrane_tee_plugin,
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.12",
       name: "Membrane Tee Plugin",
       description: "Plugin for splitting data from a single input to multiple outputs",
       package: package(),
@@ -29,7 +29,7 @@ defmodule Membrane.Tee.MixProject do
 
   defp deps do
     [
-      {:membrane_core, github: "membraneframework/membrane_core", tag: "v0.9.0-rc.0"},
+      {:membrane_core, "~> 0.9.0"},
       {:bunch, "~> 1.0"},
       {:ex_doc, "~> 0.26", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
@@ -40,7 +40,7 @@ defmodule Membrane.Tee.MixProject do
   defp package do
     [
       maintainers: ["Membrane Team"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{
         "GitHub" => @github_url,
         "Membrane Framework Homepage" => "https://membraneframework.org"
