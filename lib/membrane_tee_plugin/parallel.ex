c7126201e8b338a682005e26a550af90eb16afee
Mateusz Front <mateusz.front@swmansion.com>
2022-02-16 11:37:32 +0100

    switch to auto demands

--- lib/membrane_tee_plugin/parallel.ex
+++ lib/membrane_tee_plugin/parallel.ex
@@ -6,7 +6,7 @@ defmodule Membrane.Tee.Parallel do
 
   To use, link this element to one preceding element via `input` pad and multiple
   succesive elements via `output` pads. Each buffer is forwarded only when demand for
-  it comes in via each output.
+  it comes in via each output. If there are no outputs, buffers are dropped.
   """
 
   use Membrane.Filter
@@ -14,49 +14,17 @@ defmodule Membrane.Tee.Parallel do
   def_input_pad :input,
     availability: :always,
     mode: :pull,
-    demand_unit: :buffers,
+    demand_mode: :auto,
     caps: :any
 
   def_output_pad :output,
     availability: :on_request,
     mode: :pull,
+    demand_mode: :auto,
     caps: :any
-
-  @impl true
-  def handle_init(_) do
-    state = %{}
-    {:ok, state}
-  end
 
   @impl true
   def handle_process(:input, %Membrane.Buffer{} = buffer, _ctx, state) do
     {{:ok, forward: buffer}, state}
-  end
-
-  @impl true
-  def handle_demand(Pad.ref(:output, _id), _size, :buffers, ctx, state) do
-    {{:ok, make_demands(ctx)}, state}
-  end
-
-  @impl true
-  def handle_pad_removed(Pad.ref(:output, _id), %{playback_state: :playing} = ctx, state) do
-    {{:ok, make_demands(ctx)}, state}
-  end
-
-  @impl true
-  def handle_pad_removed(Pad.ref(:output, _id), _ctx, state) do
-    {:ok, state}
-  end
-
-  defp make_demands(ctx) do
-    minimal_size =
-      ctx.pads
-      |> Bunch.KVEnum.values()
-      |> Enum.filter(&(&1.direction == :output))
-      |> Enum.map(& &1.demand)
-      |> Enum.min(fn -> 0 end)
-      |> max(0)
-
-    [demand: {:input, minimal_size}]
   end
 end
